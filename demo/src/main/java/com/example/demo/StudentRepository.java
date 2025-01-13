39a5ca0e049abfdca5e661287f64d25b030f15ef
Tshegofatso Sekgothe <tshegofatso.sekgothe@nagarro.com>
2025-01-13 16:31:11 +0200

    added save student method need to then add h2 driver

--- demo/src/main/java/com/example/demo/StudentRepository.java
+++ demo/src/main/java/com/example/demo/StudentRepository.java
@@ -5,10 +5,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
-    
-    StudentEntity findById(long id);
-    
-    public void saveStudent(StudentEntity student);
-
-    public void deleteStudent(long id);
 }
